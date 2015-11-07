900972bf0ebe6ba5c6103d6a98898c6be53b7e2d
Biciat <carlesmadu@gmail.com>
2015-11-07 12:36:42 +0100

    Base player created

--- GameJamGame/Assets/Scripts/AliveObjectController.cs
+++ GameJamGame/Assets/Scripts/AliveObjectController.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CustomController : MonoBehaviour {
+public class AliveObjectController : MonoBehaviour {
 
 
 public Animator anim;
@@ -17,76 +17,44 @@ public GameObject hitbox;
 
 	// Use this for initialization
 	void Start () {
-	
 		anim = gameObject.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 	
 		float yin = Input.GetAxis("Vertical");
-		
-		if (yin < 0) {
-			anim.SetBool("Guard", true);
-			Guard = true;
-		} else if (Guard) {
-			anim.SetBool("Guard",false);
-			Guard = false;
-		}
-	
-		
-	
-	
 		float move = Input.GetAxis("Horizontal");
-		if (Guard) move = 0;
-		anim.SetFloat("Movement", Mathf.Abs(move));
+
+		//anim.SetFloat("Movement", Mathf.Abs(move));
 	
 		//face right or left
 		if (move != 0) {
-			Vector3 newScale = transform.localScale;
 			if (move < 0) {
-				newScale.x = -1.0f;
 				right = false;
 			}
 			else {
-				newScale.x = 1.0f;
 				right = true;
 			}
-			transform.localScale = newScale;
-			
 			move *= Time.deltaTime*speed;
 			this.transform.position += new Vector3(move,0.0f,0.0f);
 		}
 
-	
+		
 		if (Input.GetKeyDown(KeyCode.Space)) {
 			if (!onAir) {
 				anim.SetBool("onAir",true);
-				GetComponent<Rigidbody2D>().AddForce(transform.up * jumpPower);
+				GetComponent<Rigidbody>().AddForce(transform.up * jumpPower);
 				onAir = true;
 			} else if (!dJump) {
 				//anim.SetTrigger("Jump");
-				GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x,0);
-				GetComponent<Rigidbody2D>().AddForce(transform.up * jumpPower);
+				GetComponent<Rigidbody>().velocity = new Vector2(GetComponent<Rigidbody>().velocity.x,0);
+				GetComponent<Rigidbody>().AddForce(transform.up * jumpPower);
 				dJump = true;
 			}
 		}
-	
-		if (Input.GetKeyDown(KeyCode.Q) && !onAir ) {
-			doBazooka();
-		
-		}
-	
 	}
-	
-	
-	private void doBazooka()
-	{
-		anim.SetTrigger("DoBazooka");
-	
-	}
-	
-	
+
 	private void readyHit() 
 	{
 		int r = 1;
@@ -106,19 +74,19 @@ public GameObject hitbox;
 	}
 	
 	
-	void OnCollisionEnter2D(Collision2D crash) {
-		if (crash.gameObject.tag == "Floor") { 		
+	void OnCollisionEnter(Collision crash) {
+		if (crash.gameObject.layer == LayerMask.NameToLayer("Floor")){ 		
 			if (onAir) {
-				anim.SetBool("onAir",false);
+				//anim.SetBool("onAir",false);
 				onAir = false;
 				dJump = false;
 			}
 		}
 	  }
 	
-	void OnCollisionExit2D(Collision2D coll) {
-		if (coll.gameObject.tag == "Floor") { 		
-				anim.SetBool("onAir",true);
+	void OnCollisionExit(Collision coll) {
+		if (coll.gameObject.layer == LayerMask.NameToLayer("Floor")) { 		
+				//anim.SetBool("onAir",true);
 				onAir = true;
 				dJump = false;
 		}
